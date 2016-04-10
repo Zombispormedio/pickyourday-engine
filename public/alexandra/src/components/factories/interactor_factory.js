975be0d2925fa16d7a02841b86a8e308d6c962a1
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-10 18:25:26 +0200

    Example. Streaming

--- public/alexandra/src/components/factories/interactor_factory.js
+++ public/alexandra/src/components/factories/interactor_factory.js
@@ -1,120 +1,112 @@
 angular.module('alexandra')
     .factory('$alexandraInteractor', function($window){
 
-    var element, camera, 
-        dragging=false,
-        x=0, y=0, lastX=0, lastY=0, 
-        button=0, ctrl=false, key=0,
-        dstep=0, dloc=0,
-        MOTION_FACTOR=10.0;
-
-
-
-    var OnMouseUp=function(ev){
-        dragging=false;
-    }
-
-    var OnMouseDown=function(ev){
-        dragging=true;
-        x=ev.clientX;
-        y=ev.clientY;
-        button=ev.button;
-
-        var c_position=camera.getPosition();
-
-        dstep=Math.max(c_position[0], c_position[1], c_position[2])/100;
-    }
-
-
-    var OnMouseMove=function(ev){
-        lastX=x;
-        lastY=y;
-        x=ev.clientX;
-        y=ev.clientY;
-        if(!dragging)return;
-
-        ctrl=ev.ctrlKey;
-        alt=ev.altKey;
-
-        var dx=x-lastX;
-        var dy=y-lastY;
-
-        if(button===0){
-            if(ctrl){
-                pan(dx, dy);
-            }else{
-                rotate(dx, dy); 
-            }
-        }
-
-    }
-    
-    function pan(dx, dy){
-        console.log(dx);
-        console.log(dy);
-    }
-
-    function rotate(dx, dy){
-        var delta_elevation=-20.0/element[0].height;
-        var delta_azimuth=-20.0/element[0].width;
-
-        var nAzimuth=dx*delta_azimuth*MOTION_FACTOR;
-        var nElevation=dy*delta_elevation*MOTION_FACTOR;
-
-        camera.changeAzimuth(nAzimuth);
-        camera.changeElevation(nElevation);
-    }
-
-
-    function dolly(dy){
-        if(dy>0){
-            dloc+=dstep;
-        }else{
-            dloc-=dstep;
-        }
-        camera.zoom(dloc);
-    }
-
-
-    var OnKeyDown=function(ev){
-        key=ev.keyCode;
-        ctrl=ev.ctrlKey;
-
-        if(!ctrl){
-            if(key===38){
-                camera.changeElevation(10);
-            }
-            else if(key===40){
-                camera.changeElevation(-10);
-            }
-            else if(key===37){
-                camera.changeAzimuth(-10);
-            }
-            else if(key===39){
-                camera.changeAzimuth(10);
-            }
-        }
-
-    }
-
-    var OnKeyUp=function(ev){
-        if(ev.keyCode==17){
-            ctrl=false;
-        }
-    }
-
-    var OnWheel=function(ev){
-        dolly(ev.wheelDeltaY);
-    }
-    
-    
-   
-
-
-
     return function(e, c){
-        element=e;
-        camera=c;
+
+        var element=e, camera=c, 
+            dragging=false,
+            x=0, y=0, lastX=0, lastY=0, 
+            button=0, ctrl=false, key=0,
+            dstep=0, dloc=0,
+            MOTION_FACTOR=10.0;
+
+        var OnMouseUp=function(ev){
+            dragging=false;
+        }
+
+        var OnMouseDown=function(ev){
+            dragging=true;
+            x=ev.clientX;
+            y=ev.clientY;
+            button=ev.button;
+
+            var c_position=camera.getPosition();
+
+            dstep=Math.max(c_position[0], c_position[1], c_position[2])/100;
+        }
+
+
+        var OnMouseMove=function(ev){
+            lastX=x;
+            lastY=y;
+            x=ev.clientX;
+            y=ev.clientY;
+            if(!dragging)return;
+
+            ctrl=ev.ctrlKey;
+            alt=ev.altKey;
+
+            var dx=x-lastX;
+            var dy=y-lastY;
+
+            if(button===0){
+                if(ctrl){
+                    dolly(dx, dy);
+                }else{
+                    rotate(dx, dy); 
+                }
+            }
+
+        }
+
+
+
+        function rotate(dx, dy){
+            var delta_elevation=-20.0/element[0].height;
+            var delta_azimuth=-20.0/element[0].width;
+
+            var nAzimuth=dx*delta_azimuth*MOTION_FACTOR;
+            var nElevation=dy*delta_elevation*MOTION_FACTOR;
+
+            camera.changeAzimuth(nAzimuth);
+            camera.changeElevation(nElevation);
+        }
+
+
+        function dolly(dy){
+            if(dy>0){
+                dloc+=dstep;
+            }else{
+                dloc-=dstep;
+            }
+            camera.zoom(dloc);
+        }
+
+
+        var OnKeyDown=function(ev){
+            key=ev.keyCode;
+            ctrl=ev.ctrlKey;
+
+            if(!ctrl){
+                if(key===38){
+                    camera.changeElevation(10);
+                }
+                else if(key===40){
+                    camera.changeElevation(-10);
+                }
+                else if(key===37){
+                    camera.changeAzimuth(-10);
+                }
+                else if(key===39){
+                    camera.changeAzimuth(10);
+                }
+            }
+
+        }
+
+        var OnKeyUp=function(ev){
+            if(ev.keyCode==17){
+                ctrl=false;
+            }
+        }
+
+        var OnWheel=function(ev){
+            dolly(ev.wheelDeltaY);
+        }
+
+
+
+
         element.bind("mousedown", OnMouseDown);
         element.bind("mouseup", OnMouseUp);
         element.bind("mousemove", OnMouseMove);
@@ -123,6 +115,8 @@ angular.module('alexandra')
         var $=angular.element($window);
         $.bind("keydown", OnKeyDown);
         $.bind("keyup", OnKeyUp);
+
+
     }
 
 });
