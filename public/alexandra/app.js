5b65b6790ebdd7bc0b7091f286e7d90445e193fc
Xavier Serrano <zombispormedio007@gmail.com>
2016-05-12 20:47:55 +0200

    art

--- public/alexandra/app.js
+++ public/alexandra/app.js
@@ -3,22 +3,76 @@ var app = angular.module('alexandraExample', [ "alexandra"]);
 angular.module('alexandraExample')
     .controller('AlexandraExampleController', function($rootScope, $scope, $interval, $timeout, $alexandraModel) {
 
-       
+
     $scope.data=[{
-        mesh:Torus(20, 6, 100, 16),
-        position:[100,50,100],
+        mesh:$alexandraModel.TorusKnot(20, 6, 100, 16),
+        position:RandPosition(),
         color: RandColor()
     },{
-        mesh:Torus(20, 6, 100, 16),
-        position:[20,50,100],
+        mesh:$alexandraModel.Box(15,15,15),
+        position:RandPosition(),
         color: RandColor()
-    }]
-    
+    },{
+        mesh:$alexandraModel.Tube(50,20,2,8, false),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Torus(20,3, 16, 100),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Tetrahedron(5),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Sphere(5),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Octahedron(5),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Lathe(10),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Icosahedron(10),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Dodecahedron(10),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Cylinder(5, 5, 20, 32),
+        position:RandPosition(),
+        color: RandColor()
+    },{
+        mesh:$alexandraModel.Plane({
+            height:200,
+            width:200,
+            w_s:50,
+            h_s:50
+        }, function(prev, item, index){
+            var x=item.x, y=item.z, z=item.y;
+
+            y=chance.floating({min:0, max:50})
+
+            prev.push(x);
+            prev.push(y);
+            prev.push(z);
+            return prev;
+        }),
+       position:[50,0,50],
+        color: RandColor()
+    }];
+
 
 
     $scope.config={
-    
-    
+
+        engine:"phong",
         colortype:"variable",
         type:"custom",
         axis:true,
--- public/alexandra/src/components/factories/model_factory.js
+++ public/alexandra/src/components/factories/model_factory.js
@@ -6,10 +6,10 @@ angular.module('alexandra')
 
     var Geometry= function (geom, t_vertex, t_faces) {
         var data = {}
-        data.v = geom.vertices.reduce(function (prev, item) {
+        data.v = geom.vertices.reduce(function (prev, item, index) {
 
             if (t_vertex) {
-                prev = transform_vertex(prev, item, index);
+                prev = t_vertex(prev, item, index);
             }else{
                 prev.push(item.x);
                 prev.push(item.y);
@@ -19,10 +19,10 @@ angular.module('alexandra')
             return prev;
         }, []);
 
-        data.iv = geom.faces.reduce(function (prev, item) {
+        data.iv = geom.faces.reduce(function (prev, item, index) {
 
             if (t_faces) {
-                prev = transform_vertex(prev, item, index);
+                prev = t_faces(prev, item, index);
             }else{
                 prev.push(item.a);
                 prev.push(item.b);
@@ -59,12 +59,107 @@ angular.module('alexandra')
 
     }
 
+    var Dodecahedron=function(rad, detail){
+        var dode= new THREE.DodecahedronGeometry(rad, detail);
+        return transformGeometry(dode);
+    }
+
+
+
+
+    var Icosahedron=function(rad, detail){
+        var dode= new THREE.IcosahedronGeometry(rad, detail);
+
+
+        return transformGeometry(dode);
+    }
+    var  Lathe=function(line,  segments, phiStart, phiLength){
+        var points = [];
+        line=line||50
+        for ( var i = 0; i < line; i ++ ) {
+            points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+        }
+        var geometry = new THREE.LatheGeometry( points,  segments, phiStart, phiLength );
+
+
+        return transformGeometry(geometry);
+    }
+
+
+    var Tetrahedron =function(radius, detail){
+        var geometry= new THREE.TetrahedronGeometry(radius, detail);
+        return transformGeometry(geometry);
+    }
+
+    var Octahedron =function(radius, detail){
+        var geometry= new THREE.OctahedronGeometry(radius, detail);
+        return transformGeometry(geometry);
+    }
+
+    var TorusKnot=function(radius, tube, tubularSegments, radialSegments, p, q){
+        var geometry= new THREE.TorusKnotGeometry(radius, tube, tubularSegments, radialSegments, p, q)
+        return transformGeometry(geometry);
+    }
+
+    var Box=function(width, height, depth, widthSegments, heightSegments, depthSegments){
+        var geometry = new THREE.BoxGeometry( width, height, depth, widthSegments, heightSegments, depthSegments);      
+        return transformGeometry(geometry);
+    }
+
+    var Cylinder=function(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength){
+        var geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength);      
+        return transformGeometry(geometry);
+    }
+
+    var Sphere =function(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength){
+        var geometry= new THREE.SphereGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength);
+        return transformGeometry(geometry);
+    }
+
+
+    var Torus =function(radius, tube, radialSegments, tubularSegments, arc){
+        var geometry= new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments, arc);
+        return transformGeometry(geometry);
+    }
+
+    var Tube=function(l, segments, radius, radiusSegments, closed){
+        var CustomSinCurve = THREE.Curve.create(
+            function ( scale ) { //custom curve constructor
+                this.scale = (scale === undefined) ? 1 : scale;
+            },
+
+            function ( t ) { //getPoint: t is between 0-1
+                var tx = t * 3 - 1.5,
+                    ty = Math.sin( 2 * Math.PI * t ),
+                    tz = t * 3 - 1.5;
+
+                return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+            }
+        );
+
+        var path = new CustomSinCurve( l );
+
+        var geometry= new THREE.TubeGeometry(path, segments, radius, radiusSegments, closed)
+        return transformGeometry(geometry);
+    }
+
 
 
     return {
         Geometry: Geometry,
         Text: Text,
-        Plane: Plane
+        Plane: Plane,
+        Dodecahedron:Dodecahedron,
+        Icosahedron :Icosahedron,
+        Lathe:Lathe,
+        Tetrahedron:Tetrahedron,
+        TorusKnot:TorusKnot,
+        Box:Box,
+        Cylinder:Cylinder,
+        Octahedron:Octahedron,
+        Sphere:Sphere,
+        Torus:Torus,
+        Tube:Tube
     }
 
 
